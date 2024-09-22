e3829975e8aa9b7775b419821caf3e979d7d8ebd
martincorestein <103467287+martincorestein@users.noreply.github.com>
2024-09-22 19:31:46 +0000

    Proyecto Excuse Generator terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["El perro", "Mi abuela", "El Loro", "Mi gato"];
+  let action = ["se comió", "rompió", "perdió", "tiró"];
+  let what = ["mi tarea", "mi teléfono", "las llaves", "el auto"];
+  let when = [
+    "antes de la clase",
+    "mientras dormía",
+    "cuando estaba en el gimnasio",
+    "durante el almuerzo"
+  ];
+
+  const excusaAleatoria = array => Math.floor(Math.random() * array.length);
+
+  let excuse =
+    who[excusaAleatoria(who)] +
+    " " +
+    action[excusaAleatoria(action)] +
+    " " +
+    what[excusaAleatoria(what)] +
+    " " +
+    when[excusaAleatoria(when)];
+
+  document.getElementById("excuse").innerHTML = "¡" + excuse + "!";
 };
